33d40266184eba4e8249ac992e2bea684fe23268
mankal-27 <mnkalkutagi@gmail.com>
2024-01-04 11:26:02 +0530

    updated and created server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,32 +1,47 @@
 const fs = require('fs');
+const http = require('http');
 
-//Blocking, Synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+////////////////////////////////////////////////////////////////
+///Files
 
-console.log(textIn);
+// //Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const textOut = `This is what we know about the avocado : ${textIn}.\n Created on ${Date.now()}`;
+// console.log(textIn);
 
-fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+// const textOut = `This is what we know about the avocado : ${textIn}.\n Created on ${Date.now()}`;
 
-console.log("File Write Successful")
+// fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
 
-//Non-Blocking , asynchronous way of writing
+// console.log("File Write Successful")
 
-fs.readFile('./txt/starthh.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log("Error", err);
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        if(err) throw err;
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            if(err) throw err;
-            console.log(data3);
+// //Non-Blocking , asynchronous way of writing
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
-                if(err) throw err;
-                console.log("File Write Successful")
-            })
-        });
-    });
+// fs.readFile('./txt/starthh.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log("Error", err);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         if(err) throw err;
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             if(err) throw err;
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+//                 if(err) throw err;
+//                 console.log("File Write Successful")
+//             })
+//         });
+//     });
+// });
+// console.log('Will Read file!')
+
+////////////////////////////////////////////
+// Server
+
+const server = http.createServer((req, res) => {
+    res.end('Hello From The Server!');
 });
-console.log('Will Read file!')
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server is running!');
+});
