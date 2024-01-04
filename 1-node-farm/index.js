d4074660e48d37a10b6f3628c3657992ccd20566
mankal-27 <mnkalkutagi@gmail.com>
2024-01-04 14:31:59 +0530

    updated route to handle undefined url

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////////////////////////////////////////////////////
 ///Files
@@ -39,7 +40,19 @@ const http = require('http');
 // Server
 
 const server = http.createServer((req, res) => {
-    res.end('Hello From The Server!');
+    console.log(req.url);
+
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    }else if(pathName === '/product'){
+        res.end('This is the product');
+    }else{
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        })
+        res.end('<h1>Page Not Found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
