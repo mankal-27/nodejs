84321fe1431d469e3e182d659b466839c1379781
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 15:58:57 +0530

    es6 module export

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -9,3 +9,6 @@ console.log(calc1.add(2,4));
 //exports
 const calc2 = require('./test-module-1');
 console.log(calc2.add(2,8));
+
+const {add, multiply, divide} = require('./test-module-1');
+console.log(add(2,9));
