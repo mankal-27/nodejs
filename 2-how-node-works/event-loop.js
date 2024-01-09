9e5daad853302934077243bdae4f5387eb95b622
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 10:25:54 +0530

    updated for process next tick

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -4,6 +4,12 @@ setImmediate(() => console.log("Immediate 1 Finished"));
 
 fs.readFile('test-file.txt', () => {
     console.log('I/O Test File Finished');
+    console.log('================================');
+    setTimeout(() => console.log("Timer 2 Finished"), 0);
+    setTimeout(() => console.log("Timer 3 Finished"), 3000);
+    setImmediate(() => console.log("Immediate 2 Finished"));
+
+    process.nextTick(() => console.log("Process.nextTick"));
 })
 
 console.log('Hello from the top-level code');
