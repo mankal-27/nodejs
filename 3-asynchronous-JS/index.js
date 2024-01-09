bc9d74e3d65e9ac01f206078dd490d2229069041
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 20:08:56 +0530

    async anony

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -36,7 +36,7 @@ const writeFilePro = (file, data) => {
 
 const getDogPic = async () => {
     try {
-        const data = await readFilePro(`${__dirname}/doghgchc.txt`);
+        const data = await readFilePro(`${__dirname}/dog.txt`);
         console.log(`Breed : ${data}`);
         
         const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
@@ -51,10 +51,21 @@ const getDogPic = async () => {
     return '2: Ready !'
 }
 
-console.log('1: Will Get Dog Image');
-getDogPic().then((x) => {
-    console.log(x);
-    console.log('3: Done Getting Dog Image');
-}).catch((err) => {
-    console.log('Eroor came noooooo------');
-})
+// console.log('1: Will Get Dog Image');
+// getDogPic().then((x) => {
+//     console.log(x);
+//     console.log('3: Done Getting Dog Image');
+// }).catch((err) => {
+//     console.log('Eroor came noooooo------');
+// })
+
+(async() => {
+    try {
+        console.log('1: Will Get Dog Image');
+        const x = await getDogPic();
+        console.log(x);
+        console.log('3: Done Getting Dog Image');
+    } catch (error) {
+        console.log('Eroor came noooooo------');
+    }
+})()
