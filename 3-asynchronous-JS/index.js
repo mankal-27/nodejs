40d0ef7afea4b4e5de0ab9e55ed89cca0ec318c5
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 19:25:02 +0530

    Handled promisify using superagent inbuilt

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -5,13 +5,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) =>{
     console.log(`Breed : ${data}`)
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if(err) return console.log(err.message)
-        console.log(res.body)
-        
+    .then(res => {
+        console.log(res.body.message)
+
         fs.writeFile('dog-img.txt', res.body.message, err => {
             if(err) return console.log(err.message)
             console.log('Random Dog Image Saved')
-        })
+        });
     })
-})
+    .catch(err => {
+        if(err) return console.log(err.message)
+    })
+});
