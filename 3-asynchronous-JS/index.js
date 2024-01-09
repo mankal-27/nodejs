9c095d01490b143e526403417f141c5fa3847a38
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 20:18:24 +0530

    Promise all

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -39,10 +39,16 @@ const getDogPic = async () => {
         const data = await readFilePro(`${__dirname}/dog.txt`);
         console.log(`Breed : ${data}`);
         
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-        console.log(res.body.message);
+        const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+        // console.log(all);
+        const imgs = all.map(el => el.body.message)
+        console.log(imgs);
 
-        await writeFilePro('dog-img.txt',  res.body.message)
+        await writeFilePro('dog-img.txt',  imgs.join('\n'))
         console.log('Random Dog Image Saved');
     } catch (error) {
         console.log(error);
