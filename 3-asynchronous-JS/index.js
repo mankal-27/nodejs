cf15af672189cef8c6838a553467591d65efb459
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 19:43:58 +0530

    Updated promisify

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) =>{
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(err) return reject('I could not find file');
+            resolve(data)
+        })
+    })
+}
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err=>{
+            if(err) return reject('I could not write file');
+            resolve('File Written Successfully'); 
+        })
+    })
+}
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data =>{
     console.log(`Breed : ${data}`)
-    superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)})
     .then(res => {
         console.log(res.body.message)
-
-        fs.writeFile('dog-img.txt', res.body.message, err => {
-            if(err) return console.log(err.message)
-            console.log('Random Dog Image Saved')
-        });
+        return writeFilePro('dog-img.txt',  res.body.message)
+    })
+    .then(() => {
+        console.log('Random Dog Image Saved')
     })
     .catch(err => {
         if(err) return console.log(err.message)
-    })
-});
+    });
