d0c4db4a7f1b4f189fe7c14694ef955e6f26cb73
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 11:44:45 +0530

    updated event emit with server

--- 2-how-node-works/events.js
+++ 2-how-node-works/events.js
@@ -1,17 +1,46 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmmiter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
 
-myEmmiter.on('newSale', () => {
-    console.log("There was a new Sale!");
-})
+const myEmitter = new Sales();
 
-myEmmiter.on('newSale', () => {
-    console.log("Customer Name : Manjunath");
-})
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
 
-myEmmiter.on('newSale', (stock) => {
-    console.log(`There are now ${stock} Items left in Stock.`);
-})
+myEmitter.on("newSale", () => {
+  console.log("Costumer name: Jonas");
+});
 
-myEmmiter.emit('newSale', 9);
+myEmitter.on("newSale", stock => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit("newSale", 9);
+
+//////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request 😀");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
