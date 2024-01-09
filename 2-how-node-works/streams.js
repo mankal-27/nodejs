2f8149f1b53f37b4ac296ab10c6cec135d96b075
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 12:37:57 +0530

    using stream method to read data in chunks

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -3,12 +3,26 @@ const server  = require("http").createServer();
 
 server.on('request', (req, res) => {
     //Solution1 
-    fs.readFile('test-file.txt', (err, data) => {
-        if(err){
-            console.log(err);
-        }
-        res.end(data)
-    })
+    // fs.readFile('test-file.txt', (err, data) => {
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     res.end(data)
+    // })
+
+    //Solution2 : Streams
+    const readableStream = fs.createReadStream('test-file.txt');
+    readableStream.on('data', chunk => {
+        res.write(chunk);
+    });
+    readableStream.on('end', () => {
+        res.end();
+    });
+    readableStream.on('error', err => {
+        console.log(err);
+        res.statusCode = 500;
+        res.end("File not found");
+    });
 })
 
 server.listen(8000, '127.0.0.1', () => {
