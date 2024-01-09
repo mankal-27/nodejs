9eea77ac18576711cae1950dbb07e40411ef1e10
mankal-27 <mnkalkutagi@gmail.com>
2024-01-09 15:57:18 +0530

    export modules workflow

---
+++ 2-how-node-works/modules.js
@@ -0,0 +1,11 @@
+// console.log(arguments);
+// console.log(require('module').wrapper)
+
+// module.exports
+const C = require('./test-module');
+const calc1 = new C;
+console.log(calc1.add(2,4));
+
+//exports
+const calc2 = require('./test-module-1');
+console.log(calc2.add(2,8));
