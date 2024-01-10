ea1b54ed318ba195c06e4fef29b2d7f40ad7cfc4
mankal-27 <mnkalkutagi@gmail.com>
2024-01-10 21:41:59 +0530

    excercise on multiple middelware chainning

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -46,13 +46,22 @@ const getTour = (req, res) => {
         });
 };
 
+const checkBody = (req, res, next) => {
+    if(!req.body.name || !req.body.price){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Name Or Price must be provided'
+        });
+    }
+    next();
+};
 const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({
         id: newId
     }, req.body);
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         if (err) throw err;
         res.status(201).json({
             status:'success',
@@ -85,5 +94,6 @@ module.exports = {
     createTour : createTour,
     updateTour : updateTour,
     deleteTour : deleteTour,
-    checkID: checkID
+    checkID: checkID,
+    checkBody : checkBody
 }
