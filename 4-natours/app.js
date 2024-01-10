0db060178e670986a7dd472e288d8e26b7cf3600
mankal-27 <mnkalkutagi@gmail.com>
2024-01-10 15:55:30 +0530

    patch method

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -70,6 +70,23 @@ app.post('/api/v1/tours', (req, res) => {
         })
     })
 })
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    //const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status: 'Sucsess',
+        data: {
+            tour: '<Updated tour here>'
+        }
+    })
+})
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
