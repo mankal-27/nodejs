4b80521a11309ac5d1ab2888d5d94762248b7c96
mankal-27 <mnkalkutagi@gmail.com>
2024-01-11 13:02:51 +0530

    Updated Mongoose schema

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,38 @@
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+    }).then(() => {
+    console.log('Connected to MongoDB');
+    })
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A Tour must have a name'],
+        unqiue: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 // Server Listener
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
