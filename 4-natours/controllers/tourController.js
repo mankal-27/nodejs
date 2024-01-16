aad060b3485a9c84b1bbaf458da6143381c3edf5
mankal-27 <mnkalkutagi@gmail.com>
2024-01-16 19:15:41 +0530

    updated mongo query

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,91 +1,97 @@
 const fs = require('fs');
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+const Tour = require('./../models/tourModel');
 
+//** Before Implementing Mongo Database we were picking from static file below */
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+//****//
 
-const checkID = (req, res, next, val) => {
-    console.log(`Tour ID is : ${val}`);
-    if(req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    next();
-};
-
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status:'success',
-        requestedAt : req.requestTime,
-        results: tours.length,
-        data: {
-            tours
+const getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+        status: 'Success',
+        result: tours.length,
+        data:{
+            tours: tours
         }
-    });
-};
-
-const getTour = (req, res) => {
-    console.log(req.params)
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    if(!tour){
-        return res.status(404).json({
+    })
+    } catch (error) {
+        res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID'
+            message: error
         })
     }
+
     
-    res.status(200)
-        .json({
-            status:'success',
-            data : {
-                tours: tour
-            }
-        });
 };
 
-const checkBody = (req, res, next) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Name Or Price must be provided'
-        });
-    }
-    next();
+const getTour = async (req, res) => {
+   try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+        status:'success',
+        data: {
+            tour
+        }
+    })
+   } catch (error) {
+    res.status(404).json({
+        status: 'fail',
+        message: error
+    })
+   }
 };
-const createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({
-        id: newId
-    }, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        if (err) throw err;
+
+const createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
         res.status(201).json({
             status:'success',
             data: {
                 tour: newTour
             }
         })
-    })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error
+        })
+    }
+    
 };
 
-const updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'Sucsess',
-        data: {
-            tour: '<Updated tour here>'
-        }
-    })
+const updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                tour: tour
+            }
+        })
+    } catch (error) {
+       
+    }
+    
 };
 
-const deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'Sucsess',
-        data:null
-    })
+const deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: 'Sucsess',
+            data:null
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error
+        })
+    }
+    
 };
 
 module.exports = {
@@ -93,7 +99,5 @@ module.exports = {
     getTour : getTour,
     createTour : createTour,
     updateTour : updateTour,
-    deleteTour : deleteTour,
-    checkID: checkID,
-    checkBody : checkBody
+    deleteTour : deleteTour
 }
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -12,45 +12,9 @@ mongoose
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-    }).then(() => {
-    console.log('Connected to MongoDB');
-    })
+    }).then(() => {console.log('Connected to MongoDB')});
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-      },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-    name: 'Belgaum Beach',
-    price: 1344,
-    rating: 4.3
-});
-
-testTour
-    .save()
-    .then(doc => {
-    console.log(doc);
-}).catch(err => {
-    if (err.code === 11000) {
-        console.log('Duplicate key error:', err.message);
-    } else {
-        console.log('Error while saving:', err);
-    }
-})
 // Server Listener
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
